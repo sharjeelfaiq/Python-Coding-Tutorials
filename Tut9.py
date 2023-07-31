667c8cf71f423b4e674811c9a216667869640463
Sharjeel Faiq <mrmalik610@gmail.com>
2023-07-31 22:59:25 +0500

    Completed Tut9.py

---
+++ Tut9.py
@@ -0,0 +1,17 @@
+# Boolean Values
+x = 5
+y = 10
+is_greater = x > y
+is_equal = x == y
+is_less = x < y
+# print(is_less)
+
+# Example: Most Values are True
+def is_true(value):
+    if value:
+        return bool(value)
+    else:
+        return bool(value)
+
+result = is_true({})
+print(result)
