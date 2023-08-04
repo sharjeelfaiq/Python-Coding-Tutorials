c3e56748329b899a90a6ed4da3547938c5db576a
Sharjeel Faiq <mrmalik610@gmail.com>
2023-08-04 22:27:50 +0500

    Tut10.py Completed

--- Tut10.py
+++ Tut10.py
@@ -32,3 +32,18 @@ y = x
 z = [1, 2, 3]
 is_same_object = x is not y
 print(is_same_object) """
+
+# Python Membership Operators Operators
+""" x = [1, 2.5, 3, 4, 5]
+y = 2.5 in x
+print(y) """
+
+# Python Bitwise Operators
+a = 5 # 0101 in binary
+b = 3 # 0011 in binary
+
+bitwise_and = a & b # 0001 in binary
+bitwise_or = a | b # 0111 in binary
+bitwise_xor = a ^ b # 0110 in binary
+bitwise_not = ~a # 1010 in binary
+print(bitwise_not)
